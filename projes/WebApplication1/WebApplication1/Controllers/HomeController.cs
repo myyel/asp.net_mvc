f847a25ff0d8fce6aa19f6c4ec524dcfa8af5797
myyel <mehmetyasinyel@hotmail.com>
2021-03-14 00:52:17 +0300

    asp.net_learn04

--- projes/WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ projes/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -11,7 +12,21 @@ namespace WebApplication1.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            List<urun> urunler = new List<urun>()
+            {
+                new urun(){urun_id=1,urun_adi="İphone 5", fiyat=2000, aciklama="idare eder", satista_mi=true},
+                new urun(){urun_id=2,urun_adi="İphone 6", fiyat=3000, aciklama="idare eder", satista_mi=true},
+                new urun(){urun_id=3,urun_adi="İphone 7", fiyat=4000, aciklama="idare eder", satista_mi=false},
+                new urun(){urun_id=4,urun_adi="İphone 8", fiyat=5000, aciklama="idare eder", satista_mi=true},
+                new urun(){urun_id=5,urun_adi="Samsung 5", fiyat=2000, aciklama="idare eder", satista_mi=true},
+                new urun(){urun_id=6,urun_adi="Samsung 6", fiyat=3000, aciklama="idare eder", satista_mi=true},
+                new urun(){urun_id=7,urun_adi="Samsung 7", fiyat=4000, aciklama="idare eder", satista_mi=false},
+                new urun(){urun_id=8,urun_adi="Samsung 8", fiyat=5000, aciklama="idare eder", satista_mi=true},
+            };
+
+            ViewBag.urun_sayisi = urunler.Count();
+
+            return View(urunler);
         }
     }
 }
---
+++ projes/WebApplication1/WebApplication1/Models/urun.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class urun
+    {
+        public int urun_id { get; set; }
+        public string urun_adi { get; set; }
+        public string aciklama { get; set; }
+        public int fiyat { get; set; }
+        public bool satista_mi { get; set; }
+    }
+}
